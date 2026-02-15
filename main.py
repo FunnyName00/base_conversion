25febe35d01686cc4226630ecfce2221fb747fbf
FunnyName <emi.houdart@student.uclouvain.be>
2026-02-15 09:48:30 +0100

    added lstToInt, made decimalToBinary return an int + added docstrings

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 
 def binTo10(number):
+    """
+    Function to convert from a binary int -> decimal int
+    
+    :param number: int
+    """
+
     final = 0
     number = str(number)
-    sum = 0 
+     
     for i in range(len(number)):
         final += int(number[len(number)-i-1])*(2**i)
-        print(number[i], " += " , int(number[i])*(2**i), " ",i)
+
     return final
 
 def decimaloBinary(number):
+    """
+    Function to convert from a decimal int -> binary int
+    
+    :param number: int
+    """
+
     temp = []
     while number >=1:
         temp.append(number % 2)
         number = number // 2
     temp.reverse()
-    return temp
+    
+    return lstToInt(temp)
+
+def lstToInt(lst):
+    """
+    Makes a integer by concatenating elements of lst
+    (Not a sum)
+    
+    :param lst: List
+    """
+    final = ""
+    for i in range(len(lst)):
+        final+=str(lst[i])
+
+    return int(final)
 
 print(binTo10(11011110))  #-> 222  
 print(decimaloBinary(222))  #-> 11011110
