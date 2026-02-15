25fef9beaefefd8be60b8cdad47cf426174233d0
FunnyName <emi.houdart@student.uclouvain.be>
2026-02-15 09:39:19 +0100

    added decimaltoBinary func

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ def binTo10(number):
         print(number[i], " += " , int(number[i])*(2**i), " ",i)
     return final
 
-print(binTo10(11011110))   
+def decimaloBinary(number):
+    temp = []
+    while number >=1:
+        temp.append(number % 2)
+        number = number // 2
+    temp.reverse()
+    return temp
+
+print(binTo10(11011110))  #-> 222  
+print(decimaloBinary(222))  #-> 11011110
